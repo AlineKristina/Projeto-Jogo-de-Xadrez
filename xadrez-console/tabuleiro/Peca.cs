2380ac80877a108381a3ba36937937b350bf35e1
Aline Cristina Costa da Silva <aline.cristina.2k17@outlook.com>
2021-09-07 18:23:14 -0300

    Testes iniciais com movimentos possíveis das pecas

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace tabuleiro
 {
-    class Peca
+    abstract class Peca
     {
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
@@ -19,12 +19,7 @@ namespace tabuleiro
             this.qteMovimento = 0;
         }
 
-        public void incrementarQteMovimentos()
-        {
-            qteMovimento++;
-        }
-
-
+        public abstract bool[,] movimentosPossivels();
 
 
     }
